eb0a8188735fc13fcb9591575d02ea90b52fcfac
Aldair Meneses <aldair.meneses@proton.me>
2024-01-27 21:42:59 -0300

    Initial Release

---
+++ src/js/common.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetElement = document.querySelector(this.getAttribute('href'));
+
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - 50, // Ajuste esse valor conforme necessário
+                behavior: 'smooth'
+            });
+        }
+    });
+});
